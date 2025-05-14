df4c2ed8859e4570c2b55b47f74c469f4f2b9ad6
Thiago Akira <thiagoakira00@gmail.com>
2025-05-14 18:13:58 -0300

    Criando Categorias

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -3,6 +3,7 @@ import { MaterialIcons } from "@expo/vector-icons"
 
 import { colors } from "@/styles/colors"
 import { styles } from "./styles"
+import { categories } from "@/utils/categories"
 
 import { Category } from "@/components/category"
 
@@ -17,7 +18,7 @@ export default function Index(){
                 </TouchableOpacity>
             </View>
 
-            <Category name="Projeto" icon="code" isSelected/>
+            <Category name={categories[0].name} icon={categories[0].icon} isSelected/>
             <Category name="Site" icon="language" isSelected={false}/>
             <Category name="Video" icon="movie" isSelected={false}/>
         </View>
